8068d9b676ed5e12d31794e5ac772725af6ba27d
Braulio Bhavamitra <braulio@eita.org.br>
2014-04-02 14:05:37 -0300

    Make variables optional

--- symlink.sh
+++ symlink.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 THEMES_DIR=`basename $PWD`
+LINK_DEST=`dirname $PWD`
+
 THEMES_SUBDIRS=`echo {colivre-themes/themes,}`
 IGNORE_SUBDIRS="\(colivre-themes\)"
 
-DEST=`dirname $PWD`
-
-THEMES="`ls -d */ | grep -v $IGNORE_SUBDIRS` `find $THEMES_SUBDIRS/* -maxdepth 0 -type d`"
+if [[ -n $IGNORE_SUBDIRS ]]; then
+  THEMES=`ls -d */ | grep -v $IGNORE_SUBDIRS`
+else
+  THEMES=`ls -d */`
+fi
+if [[ -n $THEMES_SUBDIRS ]]; then
+  THEMES+=" `find $THEMES_SUBDIRS/* -maxdepth 0 -type d`"
+fi
 
 for theme in $THEMES; do
   echo "Linking $theme"
 
-  ln -sf $THEMES_DIR/$theme -t $DEST
+  ln -sf $THEMES_DIR/$theme -t $LINK_DEST
 done
 
